2a4db75c036243eb13811354b1f6f187fb6c9947
모진혁 <gkgk246@naver.com>
2022-07-26 21:08:16 +0900

    TDD settting 작업

--- JavaStudy/src/TDD/Movie/MovieTest.java
+++ JavaStudy/src/TDD/Movie/MovieTest.java
@@ -7,11 +7,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MovieTest {
 	
 	@Test
-	public void canCreateMovie() {
-		
+	public void should_return_0_when_just_created() {
 		Movie movie = new Movie();
 		assertThat(movie.averageRating()).isEqualTo(0);
 	}
+	
+	@Test
+	public void should_return_1_when_1_was_reated() {
+		Movie movie = new Movie();
+		movie.rate(1);
+		assertThat(movie.averageRating()).isEqualTo(1);
+	}
+	
+	
 	
 	
 
