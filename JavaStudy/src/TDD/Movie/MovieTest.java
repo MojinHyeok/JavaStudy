280ed966a8c27cae1c1532b80a7c3fdc4acdf2e1
모진혁 <gkgk246@naver.com>
2022-07-26 21:13:49 +0900

    생성자 리팩토링작업

--- JavaStudy/src/TDD/Movie/MovieTest.java
+++ JavaStudy/src/TDD/Movie/MovieTest.java
@@ -1,26 +1,35 @@
 package TDD.Movie;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MovieTest {
+	private Movie movie;
+	
+	@Before
+	public void setUp() {
+		movie =new Movie();
+	}
 	
 	@Test
 	public void should_return_0_when_just_created() {
-		Movie movie = new Movie();
 		assertThat(movie.averageRating()).isEqualTo(0);
 	}
 	
 	@Test
 	public void should_return_1_when_1_was_reated() {
-		Movie movie = new Movie();
 		movie.rate(1);
 		assertThat(movie.averageRating()).isEqualTo(1);
 	}
 	
-	
-	
+	@Test
+	public void should_return_4_when_3_and_5_was_reated() {
+		movie.rate(3);
+		movie.rate(5);
+		assertThat(movie.averageRating()).isEqualTo(4);
+	}
 	
 
 }
