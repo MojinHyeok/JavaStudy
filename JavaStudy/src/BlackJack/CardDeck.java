45724fb9e637dc49db7d8779ac710605e257168d
모진혁 <gkgk246@naver.com>
2022-08-01 22:10:38 +0900

    [객체지향이해]카드덱 구현

--- JavaStudy/src/BlackJack/CardDeck.java
+++ JavaStudy/src/BlackJack/CardDeck.java
@@ -1,13 +1,50 @@
 package BlackJack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CardDeck {
-	
+
 	private List<Card> cards;
+
+
+	private static final String[] PATTERNS = { "spade", "heart", "diamond", "club" };
+	private static final int CARD_COUNT = 13;
+
+	public CardDeck() {
+		cards = this.generateCards();
+	}
 	
+	
+	private List<Card> generateCards(){
+		List<Card> cards = new ArrayList<>();
+		for(String pattern : PATTERNS) {
+			for(int i=0;i<CARD_COUNT;i++) {
+				Card card = new Card();
+				String denomination = this.numberToDenomination(i);
+				card.setDenomination(denomination);
+				card.setPattern(pattern);
+				cards.add(card);
+			}
+		}
+		return cards;
+	}
+
+	private String numberToDenomination(int number) {
+		if (number == 1) {
+			return "A";
+		} else if (number == 11) {
+			return "J";
+		} else if (number == 12) {
+			return "Q";
+		} else if (number == 13) {
+			return "K";
+		}
+
+		return String.valueOf(number);
+	}
+
 	public Card getCard() {
 		return null;
 	}
-
 }
